0b17b26a76e5f4775520b085c08536db04de7fb3
Shakil53 <shakilahmed.dcc@gmail.com>
2023-06-20 12:03:27 +0600

    home page done and dynamically loaded data

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -3,12 +3,13 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary mb-4">
             <Container>
-                <Navbar.Brand href="#home">PANDA NEWS</Navbar.Brand>
+                <Navbar.Brand><Link to='/'>PANDA NEWS</Link></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
--- src/Shared/LeftSideComponent/LeftSideComponent.js
+++ src/Shared/LeftSideComponent/LeftSideComponent.js
@@ -19,7 +19,7 @@ const LeftSideComponent = () => {
             <div>
                 {
                     categories.map(category => <p key={category.id}>
-                        <Link to={`/category/${category.id}`}>{category.category_name}</Link>
+                        <Link to={`/category/${category.id}`}>{category.name}</Link>
                     </p>)
                 }
             </div>
